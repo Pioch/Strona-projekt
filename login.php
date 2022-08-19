114f69a6ed2b4045ed34315c49c0073919beaee3
Piotr Chrzanowski <p.chrzanowski@hydro-partner.pl>
2022-08-19 14:49:08 +0200

    Nowa wersja

--- login.php
+++ login.php
@@ -1,5 +1,26 @@
 <?php
     require('db_connection.php');
 
-    
+    $user_login = mysqli_real_escape_string($db_conn, $_POST["login"]);
+    $user_password = mysqli_real_escape_string($db_conn, $_POST["password"]);
+
+    $query_login = mysqli_query($db_conn, "SELECT * FROM users WHERE user_email ='$user_email'");
+
+    if(mysqli_num_rows($query_login) > 0) {
+        $record = mysqli_fetch_assoc($query_login);
+        $hash = $record["user_passworhash"];
+        if (password_verify($user_password, $hash)) {
+           $_SESSION["current_user"] = $record["user_id"];
+        }
+     }
+
+     session_start();
+     if (isset($_SESSION["current_user"])){
+        /* Użytkownik jest zalogowany */
+        echo "Zalogowano";
+     } else {
+        /* Użytkownik nie jest zalogowany */
+        echo "Błąd logowania";
+     }
+ 
 ?>
--- register.php
+++ register.php
@@ -1,17 +1,38 @@
 <?php
     require('db_connection.php');
 
-    $user_fullname = mysqli_real_escape_string($db_conn, $_POST["name"]);
+    $user_fullname = mysqli_real_escape_string($db_conn, $_POST["login"]);
     $user_email = mysqli_real_escape_string($db_conn, $_POST["email"]);
     $user_password = mysqli_real_escape_string($db_conn, $_POST["password"]);
+    $user_re_password = mysqli_real_escape_string($db_conn, $_POST["repassword"]);
 
     $user_passwordhash = password_hash($user_password, PASSWORD_DEFAULT);
 
-    if(mysqli_query($db_conn, "INSERT INTO users (user_fullname, user_email, user_passwordhash) VALUES('$user_fullname', '$user_email', '$user_passwordhash')")) {
-        echo "Rejestracja przebiegła poprawnie";
-    } 
-    else {
-        echo "Nieoczekiwany błąd - użytkownik już istnieje lub błąd serwera MySQL.";
+    //kilka sprawdzen co do nicku i maila
+    $spr1 = mysqli_fetch_array(mysqli_query($db_conn, "SELECT COUNT(*) FROM users WHERE user_fullname='$user_fullname' LIMIT 1")); //czy user o takim nicku istnieje
+    $spr2 = mysqli_fetch_array(mysqli_query($db_conn, "SELECT COUNT(*) FROM users WHERE user_email='$email' LIMIT 1")); // czy user o takim emailu istnieje
+
+    if($spr1 != 0) {
+        echo "Podana nazwa użytkownika już istnieje";
     }
+
+    if($spr2 != 0) {
+        echo "Taki email już istnieje";
+    }
+
+    if(strlen($user_password) < 8) {
+        echo "Hasło musi zawierać minimum 8 znaków";
+    }
+    else {
+        if($spr1 && $spr2 && $user_password == $user_re_password) {
+            if(mysqli_query($db_conn, "INSERT INTO users (user_fullname, user_email, user_passwordhash) VALUES('$user_fullname', '$user_email', '$user_passwordhash')")) {
+                echo "Rejestracja przebiegła poprawnie";
+            } 
+            else {
+                echo "Nieoczekiwany błąd - użytkownik już istnieje lub błąd serwera MySQL.";
+            }
+        }
+    }
+
     
 ?>
