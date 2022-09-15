9d99c1eb956ad739e15c13a1efbafe7ef6514dc3
Piotr Chrzanowski <p.chrzanowski@hydro-partner.pl>
2022-09-15 10:38:25 +0200

    v0.5

--- php/send_link.php
+++ php/send_link.php
@@ -1,27 +1,31 @@
 <?php
-    require('db_connection.php');
-    require('PHPMailer/src/PHPMailer.php');
-    require('PHPMailer/src/SMTP.php');
+    require "db_connection.php";
+    //require "mailer/files/db_connection2.php";
+    include "mailer/files/Exception.php";
+    include "mailer/files/PHPMailer.php";
+    include "mailer/files/SMTP.php" ;
+
 
     $input_email = mysqli_real_escape_string($db_conn, $_POST["email"]);
 
-    $query_user = mysqli_query($db_conn, "SELECT * FROM users WHERE user_email='$input_email");
+    $query_user = mysqli_query($db_conn, "SELECT * FROM users WHERE user_email='$input_email'");
     
     if(mysqli_num_rows($query_user)==1) {
         $record = mysqli_fetch_array($query_user);
-        $email=md5($record['user_email']);
+        $email= $record['user_email'];
+        $email_hash = md5($record['user_email']);
         $pass=$record['user_passwordhash'];
     
-        $link="<a href='https://partisan-armadillo-0984.dataplicity.io/reset_pass.php?key=".$email."&reset=".$pass."'>Click To Reset password</a>";
-        require_once('PHPMailer/src/PHPMailer.php');
+        $link="<a href='https://partisan-armadillo-0984.dataplicity.io/reset_pass.php?key=".$email_hash."&reset=".$pass."'>Click To Reset password</a>";
+
         $mail = new PHPMailer\PHPMailer\PHPMailer();
         $mail->IsSMTP(); // enable SMTP
-        $mail->Host = 'smtp.mailtrap.io';
+        $mail->Host = 'smtp.gmail.com';
         $mail->SMTPAuth = true;
-        $mail->Port = 2525;
-        $mail->Username = 'a49bb030cc13c7';
-        $mail->Password = 'faa73316395cb0';
-    
+        $mail->Port = 465;
+        $mail->Username = 'pitertest0408@gmail.com';
+        $mail->Password = 'pbfhtruljczcbbtg';
+        $mail->SMTPSecure = "ssl"; 
         $mail->From='pitertest0408@gmail.com';
         $mail->FromName='Piotr';
         $mail->AddAddress($email);
@@ -30,13 +34,16 @@
         $mail->Body = 'Click On This Link to Reset Password '.$pass.'';
         if($mail->Send())
         {
-          echo 'Sprawdź meila i kliknij na otrzymane łącze';
+          echo 'Sprawdź maila i kliknij na otrzymane łącze';
         }
         else
         {
           echo "Mail Error - >".$mail->ErrorInfo;
-        }    
+        }  
 
+    }
+    else  {
+      echo 'Podano zły adres mail';
     }
    
 ?>
